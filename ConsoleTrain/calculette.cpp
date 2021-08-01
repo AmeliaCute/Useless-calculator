7c57f889f3dfa0aa46b3bb1e3e73bc11cc837371
Xelow <XELOXLEGRANDE@gmail.com>
2021-08-01 10:11:56 +0200

    Commit

---
+++ ConsoleTrain/calculette.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+
+	//Variables:
+	int nombre(0);
+	int nombreAddition(0);
+
+	cin >> nombre;
+
+	cin >> nombreAddition;
+
+	cout << "Res: " << nombre + nombreAddition << "" << endl;
+	
+	return 0;
+}
